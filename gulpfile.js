3f4ec806ab666f14d713d2eb741464c3d7288b8c
Danielle Hassid <danielle.hassid@gmail.com>
2016-11-07 15:04:45 -0800

    Revert "Rename vendor to vendor2"

--- gulpfile.js
+++ gulpfile.js
@@ -50,16 +50,16 @@ gulp.task('minify-js', function() {
         }))
 });
 
-// Copy vendor2 libraries from /node_modules into /vendor2
+// Copy vendor libraries from /node_modules into /vendor
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('vendor2/bootstrap'))
+        .pipe(gulp.dest('vendor/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('vendor2/jquery'))
+        .pipe(gulp.dest('vendor/jquery'))
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('vendor2/simple-line-icons'))
+        .pipe(gulp.dest('vendor/simple-line-icons'))
 
 
     gulp.src([
@@ -70,7 +70,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor2/font-awesome'))
+        .pipe(gulp.dest('vendor/font-awesome'))
 })
 
 // Run everything
